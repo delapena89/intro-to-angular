76072e3f4b05d8eb517d1753753525add569b580
Luis <delapenl@gmail.com>
2015-09-16 11:36:18 -0600

    added pictures to the page

--- reddit.js
+++ reddit.js
@@ -2,7 +2,20 @@ var app = angular.module("Reddit", []);
 
 
 app.controller("RedditController", function($scope){
-     $scope.posts = [];
+     $scope.posts = [
+     {
+      'title': "PUPPIES",
+       'author': "ME",
+       'imageURL': 'http://images.nationalgeographic.com/wpf/media-live/photos/000/347/cache/puppy-fun_34713_990x742.jpg',
+       'description': "Puppies having fun!"
+     },
+     {
+      'title': "CARS",
+       'author': "YOU",
+       'imageURL': "https://thedrivenblog.files.wordpress.com/2012/02/mp4-27_front-3-41.jpg",
+       'description': "Cool cars!"
+     }
+     ];
      $scope.addPosts = function(){
        var payload = {
        'title': $scope.title,
